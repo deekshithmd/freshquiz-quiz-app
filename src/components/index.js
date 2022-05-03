a160368b4afd2058410d1fca2ca01a3bf2c92225
Deekshith M D <70252174+deekshithmd@users.noreply.github.com>
2022-05-03 09:53:47 +0530

    Pages routes (#2)
    * feat: added pages and routes
    
    * feat: implemented routes and pages
    
    * refactor: refactored code and added images
    
    * refactor: updated data

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import { Navigation } from "./Navigation/Navigation";
+import { Footer } from "./Footer/Footer";
+import { Home } from "./Home/Home";
+import { Quiz } from "./Quiz/Quiz";
+import { Rules } from "./Rules/Rules";
+import { Result } from "./Result/Result";
+import { Category } from "./Category/Category";
+import { Error } from "./Error/Error";
+import { Answers } from "./Answers/Answers";
+
+export {
+  Navigation,
+  Footer,
+  Quiz,
+  Result,
+  Rules,
+  Home,
+  Category,
+  Error,
+  Answers,
+};
